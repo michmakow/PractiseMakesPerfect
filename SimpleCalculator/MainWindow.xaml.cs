b13156220232f3a74e739b841487993f34555456
Michal Makowski <michmakow@gmail.com>
2021-06-08 12:53:00 +0200

    Full calculator logic

--- SimpleCalculator/MainWindow.xaml.cs
+++ SimpleCalculator/MainWindow.xaml.cs
@@ -1,17 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SimpleCalculator
 {
@@ -20,9 +9,113 @@ namespace SimpleCalculator
     /// </summary>
     public partial class MainWindow : Window
     {
+        private double lastNumber, result;
+        private SelectedOperator selectedOperator;
+
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void OperationButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(ResultLabel.Content.ToString(), out lastNumber))
+            {
+                ResultLabel.Content = "0";
+            }
+
+            selectedOperator = (sender as Button).Content.ToString() switch
+            {
+                "+" => SelectedOperator.Addition,
+                "-" => SelectedOperator.Subtraction,
+                "*" => SelectedOperator.Multiplication,
+                "/" => SelectedOperator.Division,
+                _ => selectedOperator
+            };
+        }
+
+        private void NumberButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedValue = int.Parse((sender as Button).Content.ToString());
+
+            ResultLabel.Content = ResultLabel.Content.ToString().Equals("0")
+                ? $"{selectedValue}"
+                : $"{ResultLabel.Content}{selectedValue}";
+        }
+
+        private void AcButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            ResultLabel.Content = "0";
+            result = 0;
+            lastNumber = 0;
+        }
+
+        private void PercentageButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(ResultLabel.Content.ToString(), out var tempNumber))
+            {
+                tempNumber = tempNumber / 100;
+                if (lastNumber != 0) tempNumber *= lastNumber;
+                
+                ResultLabel.Content = tempNumber.ToString();
+            }
+        }
+
+        private void NegativeButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(ResultLabel.Content.ToString(), out lastNumber))
+            {
+                lastNumber *= -1;
+                ResultLabel.Content = lastNumber.ToString();
+            }
+        }
+
+        private void EqualButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(ResultLabel.Content.ToString(), out var newNumber))
+            {
+                switch (selectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        result = lastNumber + newNumber;
+                        break;
+                    case SelectedOperator.Subtraction:
+                        result = lastNumber - newNumber;
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result = lastNumber * newNumber;
+                        break;
+                    case SelectedOperator.Division:
+                        if (newNumber == 0)
+                        {
+                            MessageBox.Show("You can not divide by 0",
+                                "Wrong operation",
+                                MessageBoxButton.OK,
+                                MessageBoxImage.Error);
+                            result = 0;
+                        }
+                        else result = lastNumber / newNumber;
+                        
+                        break;
+                }
+
+                ResultLabel.Content = result.ToString();
+            }
+        }
+
+        private void PointButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (ResultLabel.Content.ToString().Contains(".")) return;
+
+            ResultLabel.Content = $"{ResultLabel.Content}.";
+        }
+    }
+
+    public enum SelectedOperator
+    {
+        Addition,
+        Subtraction,
+        Multiplication,
+        Division
     }
 }
