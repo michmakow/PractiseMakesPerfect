2ff233577233692dd75377f1cb7feb2e85eb4a92
Michal Makowski <michmakow@gmail.com>
2021-06-14 12:28:52 +0200

    Get access to VM throughout View

---
+++ WPFWeatherApp/ViewModel/WeatherVM.cs
@@ -0,0 +1,79 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using WPFWeatherApp.Annotations;
+using WPFWeatherApp.Model;
+
+namespace WPFWeatherApp.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        private string _query;
+
+        public string Query
+        {
+            get => _query;
+            set
+            {
+                _query = value;
+                OnPropertyChanged(nameof(Query));
+            }
+        }
+
+        private CurrentConditions _currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get => _currentConditions;
+            set
+            {
+                _currentConditions = value;
+                OnPropertyChanged(nameof(CurrentConditions));
+            }
+        }
+
+        private City _selectedCity;
+
+        public City SelectedCity
+        {
+            get => _selectedCity;
+            set
+            {
+                _selectedCity = value; 
+                OnPropertyChanged(nameof(SelectedCity));
+            }
+        }
+
+        public WeatherVM()
+        {
+            //Good for test purpose, we see default in designer but no after running app
+            if (DesignerProperties.GetIsInDesignMode(new DependencyObject()))
+            {
+                SelectedCity = new City()
+                {
+                    LocalizedName = "New York"
+                };
+
+                CurrentConditions = new CurrentConditions
+                {
+                    WeatherText = "Partly cloudy",
+                    Temperature = new Temperature
+                    {
+                        Metric = new Units
+                        {
+                            Value = 21
+                        }
+                    }
+                };
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
