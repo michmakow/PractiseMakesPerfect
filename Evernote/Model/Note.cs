0fe13a09fd423d957f78fd1f464630b22a314866
Michal Makowski <michmakow@gmail.com>
2021-06-18 12:49:37 +0200

    Models

---
+++ Evernote/Model/Note.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Evernote.Model
+{
+    public class Note
+    {
+        public int Id { get; set; }
+        public int NotebookId { get; set; }
+        public string Title { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime UpdateAt { get; set; }
+        public string FileLocation { get; set; }
+    }
+}
---
+++ Evernote/Model/Notebook.cs
@@ -0,0 +1,9 @@
+﻿namespace Evernote.Model
+{
+    public class Notebook
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string Name { get; set; }
+    }
+}
