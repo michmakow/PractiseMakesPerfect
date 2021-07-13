1e444892990addd5f1e9289627594938d567d4ed
Michal Makowski <michmakow@gmail.com>
2021-07-13 15:06:07 +0200

    UserControls for Notebooks and Notes

---
+++ Evernote/View/UserControls/DisplayNote.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Evernote.Model;
+
+namespace Evernote.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for DisplayNote.xaml
+    /// </summary>
+    public partial class DisplayNote : UserControl
+    {
+        public Note Note
+        {
+            get => (Note)GetValue(NoteProperty);
+            set => SetValue(NoteProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NoteProperty =
+            DependencyProperty.Register("Note", typeof(Note), typeof(DisplayNote),
+                new PropertyMetadata(null, SetValues));
+
+        private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is DisplayNote noteUserControl)
+            {
+                noteUserControl.DataContext = noteUserControl.Note;
+            }
+        }
+
+        public DisplayNote()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Evernote/View/UserControls/DisplayNotebook.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Evernote.Model;
+
+namespace Evernote.View.UserControls
+{
+    /// <summary>
+    /// Interaction logic for DisplayNotebook.xaml
+    /// </summary>
+    public partial class DisplayNotebook : UserControl
+    {
+        public Notebook Notebook
+        {
+            get => (Notebook) GetValue(NotebookProperty);
+            set => SetValue(NotebookProperty, value);
+        }
+
+        // Using a DependencyProperty as the backing store for MyProperty.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty NotebookProperty =
+            DependencyProperty.Register("Notebook", typeof(Notebook), typeof(DisplayNotebook),
+                new PropertyMetadata(null, SetValues));
+
+        private static void SetValues(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if (d is DisplayNotebook notebookUserControl)
+            {
+                notebookUserControl.DataContext = notebookUserControl.Notebook;
+            }
+        }
+
+
+        public DisplayNotebook()
+        {
+            InitializeComponent();
+        }
+    }
+}
