913e109057653f11891f2db8b19e52fdf0fad7f8
Michal Makowski <michmakow@gmail.com>
2021-07-15 15:29:16 +0200

    Show RegisterCommand fix

---
+++ Evernote/ViewModel/Commands/ShowRegisterCommand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace Evernote.ViewModel.Commands
+{
+    public class ShowRegisterCommand : ICommand
+    {
+        public LoginVM ViewModel{ get; set; }
+
+        public ShowRegisterCommand(LoginVM vm)
+        {
+            ViewModel = vm;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            ViewModel.SwitchViews();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
